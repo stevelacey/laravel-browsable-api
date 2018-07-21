333e63d0fe699ad9aa77fd654c627d04396701cf
Steve Lacey <steve@stevelacey.net>
2018-07-21 10:43:21 +0200

    Add google notranslate tag

--- resources/views/layouts/api.blade.php
+++ resources/views/layouts/api.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
+        <meta name="google" content="notranslate">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <title>@yield('title', config('browsable-api.name'))</title>
